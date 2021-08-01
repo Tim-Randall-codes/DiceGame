3e27c20faed97ad8c9735a9fc9f0091dc0adeba1
Tim Randall <trtimrandall@gmail.com>
2021-08-01 20:59:59 +1000

    Initial Commit

---
+++ LilJoeDiceGame/LilJoeDiceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  LilJoeDiceGameApp.swift
+//  LilJoeDiceGame
+//
+//  Created by Tim Randall on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct LilJoeDiceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
